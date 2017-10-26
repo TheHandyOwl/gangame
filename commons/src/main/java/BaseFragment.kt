308e79559b202a26642d64cb45984a4972890c06
Carlos <thehandyowl.io@gmail.com>
2017-10-26 18:34:57 +0200

    BaseFragement genérico preparado para inflar una vista

--- commons/src/main/java/BaseFragment.kt
+++ commons/src/main/java/BaseFragment.kt
@@ -1,5 +1,18 @@
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import tho.com.commons.R
+
 /**
  * Created by carlos on 26/10/17.
  */
-class BaseFragment {
+abstract class BaseFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view = inflater.inflate(getLayoutRedId(), container, false)
+        return view
+    }
+
+    abstract fun getLayoutRedId(): Int
 }
